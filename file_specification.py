bc5a977647d1552551d5d4ae28884ed0f2241ef3
Erik Braun <30779061+erik98671@users.noreply.github.com>
2019-09-15 08:59:24 -0700

    Add files via upload

---
+++ file_specification.py
@@ -0,0 +1,6 @@
+# file_specification.py
+source_file = "source_file.txt"
+output_file = "output_file.xlsx"
+title = "Title"
+header = ["Column A","Column B","Column C","Column D"]
+field_lengths = [0,2,12,22,122]
